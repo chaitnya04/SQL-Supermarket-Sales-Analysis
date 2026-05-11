49fe3205c6a19079a1939410d5f85520e04b92c3
chaitnya04 <chaitnyagothankar123@gmail.com>
2026-05-11 16:18:27 +0530

    Add files via upload

---
+++ Supermarket_Sales_SQL_Analysis.sql
@@ -0,0 +1,19 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# SQL Server Management Studio Solution File, Format Version 19.00
+VisualStudioVersion = 15.0.28307.421
+MinimumVisualStudioVersion = 10.0.40219.1
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Default|Default = Default|Default
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{80C11BA4-39C9-4970-BD21-DAA30C55CC53}.Default|Default.ActiveCfg = Default
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {D22A5AAD-59D9-4233-BE32-D68028A584BF}
+	EndGlobalSection
+EndGlobal
